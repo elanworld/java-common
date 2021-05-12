785e65ece6e073a2697a7b6346e5c9691ef74863
elanworld <elanworld@163.com>
2021-05-12 23:12:06 +0800

    设置文件格式

---
+++ src/main/java/com/alan/common/text/FileSuffixEnum.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021.
+ * author:Alan
+ * All rights reserved.
+ */
+
+package com.alan.common.text;
+
+import java.util.Arrays;
+
+public enum FileSuffixEnum {
+	MP_4("mp4"), MKV("mkv"), WEBM("webm"), MP_3("mp3"), WAV("wav"), SRT("srt"),;
+
+	private String suffix;
+	FileSuffixEnum(String suffix) {
+		this.suffix = suffix;
+	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+	public static String[] video() {
+		FileSuffixEnum[] video = {MP_4, MKV, WEBM};
+		return Arrays.stream(video).map(FileSuffixEnum::getSuffix).toArray(String[]::new);
+	}
+
+	public static String[] audio() {
+		FileSuffixEnum[] video = {MP_3, WAV};
+		return Arrays.stream(video).map(FileSuffixEnum::getSuffix).toArray(String[]::new);
+	}
+}
