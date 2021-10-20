65a8bcd1c73d4ee77e7244f991d238b04d8aae99
Alan <elanworld@163.com>
2021-10-20 16:48:08 +0800

    更新bili自动保存历史;调整路径

--- src/main/java/com/alan/common/system/PathsEnum.java
+++ src/main/java/com/alan/common/system/PathsEnum.java
@@ -3,30 +3,29 @@ package com.alan.common.system;
 import java.io.File;
 
 public enum PathsEnum {
-	MY_DB("G:\\Alan\\Documents\\OneDrive\\Documents\\program\\resources\\data_box.db"), TEST(
-			"G:\\Alan\\Documents\\OneDrive\\Documents\\program\\resources\\test.db"), LOG_PATH(getLogPath());
+    TEST("D:\\Alan\\link\\program\\resources\\test.db"), LOG_PATH(getLogPath());
 
-	private static String logDirectory;
+    private static String logDirectory;
 
-	private String typeName;
+    private String typeName;
 
-	PathsEnum(String typeName) {
-		this.typeName = typeName;
-	}
+    PathsEnum(String typeName) {
+        this.typeName = typeName;
+    }
 
-	public String getPath() {
-		return typeName;
-	}
+    public String getPath() {
+        return typeName;
+    }
 
-	private static String getLogPath() {
-		if (logDirectory != null) {
-			return logDirectory;
-		}
-		String logDirectory = System.getenv("MyResources");
-		if (logDirectory != null) {
-			return new File(logDirectory, "javaLog.log").toString();
-		} else {
-			return "javaLog.log";
-		}
-	}
+    private static String getLogPath() {
+        if (logDirectory != null) {
+            return logDirectory;
+        }
+        String logDirectory = System.getenv("MyResources");
+        if (logDirectory != null) {
+            return new File(logDirectory, "javaLog.log").toString();
+        } else {
+            return "javaLog.log";
+        }
+    }
 }
