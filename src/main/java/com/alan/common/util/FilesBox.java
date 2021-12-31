5e651745bbf4440c2caf7bec3422f8df0fbc4978
Alan <elanworld@163.com>
2021-12-31 17:42:21 +0800

    维护opencv ffmpeg智能剪辑

--- src/main/java/com/alan/common/util/FilesBox.java
+++ src/main/java/com/alan/common/util/FilesBox.java
@@ -14,237 +14,237 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FilesBox {
-	static int fileGrowth = 0;
-	static List<String> tempFiles = new ArrayList<>();
+    static int fileGrowth = 0;
+    static List<String> tempFiles = new ArrayList<>();
 
-	public static String[] pathSplit(String path) {
-		File file = new File(path);
-		String name = file.getName();
-		String parent = file.getParent();
-		String[] split = name.split("\\.");
-		String ext = "";
-		String basename = "";
-		if (split.length >= 2) {
-			ext = "." + split[1];
-		}
-		if (split.length >= 1) {
-			basename = split[0];
-		}
-		String[] strings = {parent, basename, ext, name};
-		return Arrays.stream(strings).map(str -> str == null ? "" : str).toArray(String[]::new);
-	}
+    public static String[] pathSplit(String path) {
+        File file = new File(path);
+        String name = file.getName();
+        String parent = file.getParent();
+        String[] split = name.split("\\.");
+        String ext = "";
+        String basename = "";
+        if (split.length >= 2) {
+            ext = "." + split[1];
+        }
+        if (split.length >= 1) {
+            basename = split[0];
+        }
+        String[] strings = {parent, basename, ext, name};
+        return Arrays.stream(strings).map(str -> str == null ? "" : str).toArray(String[]::new);
+    }
 
-	public static String outFile(String inputPath, String add) {
-		String[] paths = pathSplit(inputPath);
-		Path outPath = Paths.get(paths[0], paths[1] + "_" + add + paths[2]);
-		return outPath.toString();
-	}
+    public static String outFile(String inputPath, String add) {
+        String[] paths = pathSplit(inputPath);
+        Path outPath = Paths.get(paths[0], paths[1] + "_" + add + paths[2]);
+        return outPath.toString();
+    }
 
-	public static String outFile(String inputPath) {
-		fileGrowth++;
-		return outFile(inputPath, String.valueOf(fileGrowth));
-	}
+    public static String outFile(String inputPath) {
+        fileGrowth++;
+        return outFile(inputPath, String.valueOf(fileGrowth));
+    }
 
-	public static String changeExt(String inputPath, String ext) {
-		String[] paths = pathSplit(inputPath);
-		Path outPath = Paths.get(paths[0], paths[1] + "." + ext);
-		return outPath.toString();
-	}
+    public static String changeExt(String inputPath, String ext) {
+        String[] paths = pathSplit(inputPath);
+        Path outPath = Paths.get(paths[0], paths[1] + "." + ext);
+        return outPath.toString();
+    }
 
-	public static String outBasename(String inputPath) {
-		String[] strings = pathSplit(inputPath);
-		return strings[1];
-	}
+    public static String outBasename(String inputPath) {
+        String[] strings = pathSplit(inputPath);
+        return strings[1];
+    }
 
-	public static String outDir(String inputFile, String add) {
-		if (add == null) {
-			add = "out";
-		}
-		File file = new File(new File(inputFile).getParent(), add);
-		if (!file.exists()) {
-			file.mkdirs();
-		}
-		return file.getAbsolutePath();
-	}
+    public static String outDir(String inputFile, String add) {
+        if (add == null) {
+            add = "out";
+        }
+        File file = new File(new File(inputFile).getParent(), add);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        return file.getAbsolutePath();
+    }
 
-	public static boolean regexFiles(String dir, String regex) {
-		boolean got = false;
-		List<String> strings = directoryList(dir);
-		List<String> found = findGroup(strings, regex);
-		if (found.size() > 0) {
-			got = true;
-		}
-		return got;
-	}
+    public static boolean regexFiles(String dir, String regex) {
+        boolean got = false;
+        List<String> strings = directoryList(dir);
+        List<String> found = findGroup(strings, regex);
+        if (found.size() > 0) {
+            got = true;
+        }
+        return got;
+    }
 
-	public static String standFileName(String inputPath) {
-		String[] rep = {"|", ">", "<", ":", "*", "?", "/", "\\"};
-		for (String s : rep) {
-			inputPath = inputPath.replace(s, "");
-		}
-		return inputPath;
-	}
+    public static String standFileName(String inputPath) {
+        String[] rep = {"|", ">", "<", ":", "*", "?", "/", "\\"};
+        for (String s : rep) {
+            inputPath = inputPath.replace(s, "");
+        }
+        return inputPath;
+    }
 
-	/**
-	 * generate new filenames time by time
-	 *
-	 * @param inputPath
-	 * @return
-	 */
-	public static String outDirFile(String inputPath) {
-		fileGrowth += 1;
-		String[] paths = pathSplit(inputPath);
-		Path outPath = Paths.get(paths[0], paths[1] + "_" + fileGrowth + paths[2]);
-		Path parent = outPath.getParent();
-		if (!Files.exists(parent)) {
-			File file = new File(parent.toString());
-			file.mkdirs();
-		}
-		return outPath.toString();
-	}
+    /**
+     * generate new filenames time by time
+     *
+     * @param inputPath
+     * @return
+     */
+    public static String outDirFile(String inputPath) {
+        fileGrowth += 1;
+        String[] paths = pathSplit(inputPath);
+        Path outPath = Paths.get(paths[0], paths[1], paths[1] + "_" + fileGrowth + paths[2]);
+        Path parent = outPath.getParent();
+        if (!Files.exists(parent)) {
+            File file = new File(parent.toString());
+            file.mkdirs();
+        }
+        return outPath.toString();
+    }
 
-	public static String temp(String inputFile) {
-		tempFiles.add(inputFile);
-		return inputFile;
-	}
+    public static String temp(String inputFile) {
+        tempFiles.add(inputFile);
+        return inputFile;
+    }
 
-	public static void tempDelete() {
-		tempFiles.forEach(file -> new File(file).delete());
-		tempFiles.clear();
-	}
+    public static void tempDelete() {
+        tempFiles.forEach(file -> new File(file).delete());
+        tempFiles.clear();
+    }
 
-	public static List<String> directoryList(String dir) {
-		return directoryListFilter(dir, false);
-	}
+    public static List<String> directoryList(String dir) {
+        return directoryListFilter(dir, false);
+    }
 
-	public static List<String> directoryListFilter(String dir, boolean walk, String... filters) {
+    public static List<String> directoryListFilter(String dir, boolean walk, String... filters) {
 
-		ArrayList<String> list = new ArrayList<>();
-		File fileDir = new File(dir);
-		if (!fileDir.isDirectory()) {
-			return list;
-		}
-		Path path = fileDir.toPath();
-		try {
-			Stream<Path> pathStream;
-			if (walk) {
-				pathStream = Files.walk(path);
-			} else {
-				pathStream = Files.list(path);
-			}
-			List<String> paths = pathStream.map(Path::toString).collect(Collectors.toList());
-			for (Object p : paths) {
-				String fileObject = p.toString();
-				if (new File(fileObject).isFile()) {
-					if (filters.length == 0) {
-						list.add(p.toString());
-					} else {
-						for (String reg : filters) {
-							if (fileObject.matches(".*" + reg + ".*")) {
-								list.add(p.toString());
-								break;
-							}
-						}
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
+        ArrayList<String> list = new ArrayList<>();
+        File fileDir = new File(dir);
+        if (!fileDir.isDirectory()) {
+            return list;
+        }
+        Path path = fileDir.toPath();
+        try {
+            Stream<Path> pathStream;
+            if (walk) {
+                pathStream = Files.walk(path);
+            } else {
+                pathStream = Files.list(path);
+            }
+            List<String> paths = pathStream.map(Path::toString).collect(Collectors.toList());
+            for (Object p : paths) {
+                String fileObject = p.toString();
+                if (new File(fileObject).isFile()) {
+                    if (filters.length == 0) {
+                        list.add(p.toString());
+                    } else {
+                        for (String reg : filters) {
+                            if (fileObject.matches(".*" + reg + ".*")) {
+                                list.add(p.toString());
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 
-	public static void move(String source, String dir) {
-		String[] paths = pathSplit(source);
-		File file = new File(dir, paths[1] + paths[2]);
-		rename(source, file.getAbsolutePath());
-	}
+    public static void move(String source, String dir) {
+        String[] paths = pathSplit(source);
+        File file = new File(dir, paths[1] + paths[2]);
+        rename(source, file.getAbsolutePath());
+    }
 
-	public static boolean rename(String source, String target) {
-		return new File(source).renameTo(new File(target));
-	}
+    public static boolean rename(String source, String target) {
+        return new File(source).renameTo(new File(target));
+    }
 
-	public static void deleteFiles(String deleteFile) {
-		try {
-			Files.deleteIfExists(Paths.get(deleteFile));
-		} catch (Exception e) {
-			Output.print(e.getMessage());
-		}
-	}
+    public static void deleteFiles(String deleteFile) {
+        try {
+            Files.deleteIfExists(Paths.get(deleteFile));
+        } catch (Exception e) {
+            Output.print(e.getMessage());
+        }
+    }
 
-	public static String renameIfLike(String origin, String likePath, double likePercent) {
-		String[] origins = pathSplit(origin);
-		String[] likePaths = pathSplit(likePath);
-		if (origins[1].equals(likePaths[1])) {
-			return likePath;
-		}
-		if (StringBox.likePercent(origins[1], likePaths[1]) > likePercent) {
-			File file = new File(likePaths[0], origins[1] + likePaths[2]);
-			new File(likePath).renameTo(file);
-			return file.getAbsolutePath();
-		}
-		return null;
-	}
+    public static String renameIfLike(String origin, String likePath, double likePercent) {
+        String[] origins = pathSplit(origin);
+        String[] likePaths = pathSplit(likePath);
+        if (origins[1].equals(likePaths[1])) {
+            return likePath;
+        }
+        if (StringBox.likePercent(origins[1], likePaths[1]) > likePercent) {
+            File file = new File(likePaths[0], origins[1] + likePaths[2]);
+            new File(likePath).renameTo(file);
+            return file.getAbsolutePath();
+        }
+        return null;
+    }
 
-	public static String inputIfNotExists(String file) {
-		if (Files.notExists(Paths.get(file))) {
-			while (true) {
-				String input = StringBox.input();
-				if (Files.exists(Paths.get(input))) {
-					return input;
-				}
-			}
-		}
-		return file;
-	}
+    public static String inputIfNotExists(String file) {
+        if (Files.notExists(Paths.get(file))) {
+            while (true) {
+                String input = StringBox.input();
+                if (Files.exists(Paths.get(input))) {
+                    return input;
+                }
+            }
+        }
+        return file;
+    }
 
-	public static boolean writer(String text, String filePath) {
-		try {
-			BufferedWriter writer = new BufferedWriter(
-					new OutputStreamWriter(new FileOutputStream(filePath), StandardCharsets.UTF_8));
-			writer.write(text);
-			writer.close();
-			return true;
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return false;
-	}
+    public static boolean writer(String text, String filePath) {
+        try {
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(filePath), StandardCharsets.UTF_8));
+            writer.write(text);
+            writer.close();
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
 
-	public static boolean writer(List<String> text, String fileName) {
-		String join = String.join("\n", text);
-		return writer(join, fileName);
-	}
+    public static boolean writer(List<String> text, String fileName) {
+        String join = String.join("\n", text);
+        return writer(join, fileName);
+    }
 
-	public static List<String> reader(String fileName) {
-		ArrayList<String> lines = new ArrayList<String>();
-		try {
-			BufferedReader br = new BufferedReader(
-					new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8));
-			String line;
-			while ((line = br.readLine()) != null) {
-				lines.add(line);
-			}
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return lines;
-	}
+    public static List<String> reader(String fileName) {
+        ArrayList<String> lines = new ArrayList<String>();
+        try {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8));
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
 
-	public static <T> T readObject(String file, Class<T> clazz) {
-		try {
-			ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
-			Object object = objectInputStream.readObject();
-			return (T) object;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public static <T> T readObject(String file, Class<T> clazz) {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
+            Object object = objectInputStream.readObject();
+            return (T) object;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	public static void writeObject(Object object, String file) throws IOException {
-		ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
-		objectOutputStream.writeObject(object);
-	}
+    public static void writeObject(Object object, String file) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
+        objectOutputStream.writeObject(object);
+    }
 }
--- src/test/java/com/alan/common/util/FilesBoxTest.java
+++ src/test/java/com/alan/common/util/FilesBoxTest.java
@@ -1,11 +1,28 @@
 package com.alan.common.util;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 public class FilesBoxTest {
-	@Test
-	public void print() {
-		Output.print("dd", "kk");
-	}
+    @Test
+    public void print() {
+        Output.print("a", "b");
+    }
+
+    @Test
+    public void outDirFile() {
+        String outDirFile = FilesBox.outDirFile("/a/b/c.d");
+        Assert.assertEquals(new File("/a/b/c/c_1.d").getAbsolutePath(), new File(outDirFile).getAbsolutePath());
+    }
+
+    @Test
+    public void pathSplit() {
+        String[] strings = FilesBox.pathSplit("/a/b/c.d");
+        Assert.assertEquals(new File("/a/b").getAbsolutePath(),new File(strings[0]).getAbsolutePath());
+        Assert.assertEquals("c",strings[1]);
+        Assert.assertEquals(".d",strings[2]);
+    }
 
 }
